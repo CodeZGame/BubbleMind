fecf3699f4096644dd662fda72eb6943f172097a
Salido Maxime <maxime.salido@gmail.com>
2013-03-06 12:18:17 +0100

    Combobox style

--- src/client/ui.js
+++ src/client/ui.js
@@ -1,27 +1,20 @@
-//$(function() {
-    //        $("#SpeedSlider").slider(
-    //        {
-    //            range: "min",
-    //            value: 30,
-    //            min: 0,
-    //            max: 50,
-    //            step: 10,
-    //            slide: function(event, ui) {
-    //    //            $("#amount").text(ui.value);
-    //            }
-    //        });
-    //        $("#amount").text($("#SpeedSlider").slider("value"));
-    //    $("#SpeedSlider").sliderWithScale(
-    //    {
-    //        range: "min",
-    //        value: 30,
-    //        min: 0,
-    //        max: 50,
-    //        step: 10
-    //                slide: function(event, ui) {
-    //            $("#amount").text(ui.value);
-    //
-    // initialize with default options
-    
-//    $( "#my-widget1" ).sliderWithScale();
-//});
+$(function() {
+    runProcessing();
+    build_slider('#timeSlider');
+    $( "#speedSlider" ).slider({
+        orientation: "vertical",
+        min: 1,
+        max: 25,
+        value: 5,
+        animate: "slow"
+    });
+
+
+
+
+    $("#selectAxeXValue").combobox();
+    $("#selectAxeXType").combobox();
+    $("#selectAxeYValue").combobox();
+    $("#selectAxeYType").combobox();
+
+});
