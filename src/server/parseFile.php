64944f91f0c7649df33b1f551c6840e043b467aa
Andral Laurent <laurent.andral@epitech.eu>
2013-04-01 18:26:59 +0200

    fix bug

--- src/server/parseFile.php
+++ src/server/parseFile.php
@@ -2,31 +2,37 @@
 require_once 'Parser.php';
 require_once 'DataInserter.php';
 
-$parser = new Parser();
-$insert = new DataInserter();
-
-foreach($_FILES as $file)
-{
-	if ($file["error"] > 0)
+if (isset($_POST["format"])) {
+	
+	foreach($_FILES as $file)
 	{
-		echo "Error: please check you correctly selected a file.";
-	}
-	else
-	{
-		if (file_exists("upload/" . $file["name"]))
+		if ($file["error"] > 0)
 		{
-			echo "File already in database.";
+			echo "Error: please check you correctly selected a file.";
 		}
 		else
 		{
-			move_uploaded_file($file["tmp_name"], "upload/" . $file["name"]);
-			$sheet = $parser->parse("upload/" . $file["name"]);
-			// dynamic $entityColumn and $yearColumn
-			$insert->InsertData($file["name"], 0, 1, $sheet);
-			$insert->CompleteData();
-			echo "File successfully added to database.";
+			if (file_exists("upload/" . $file["name"])) {
+				echo "File already in database.";
+			}
+			else
+			{
+				$parser = new Parser();
+				$insert = new DataInserter();
+				
+				move_uploaded_file($file["tmp_name"], "upload/" . $file["name"]);
+				$sheet = $parser->parse("upload/" . $file["name"]);
+				// dynamic $entityColumn and $yearColumn
+				
+				if ($_POST["format"] == "OCDE")
+					$insert->InsertOCDE($file["name"], $sheet);
+				else
+					$insert->InsertOther($file["name"], 0, 1, $sheet);
+				$insert->CompleteData();
+				echo "File successfully added to database.";
+			}
+			break;
 		}
-		break;
 	}
 }
 ?>
